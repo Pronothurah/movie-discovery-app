6727c8c8cfc18e9f911604d164971f7ad2b34855
Pronothurah <onsongoonditi@gmail.com>
2023-09-17 01:32:20 +0300

    local

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,31 +1,43 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import Aside from './Aside';
 
 
-function MovieDetails({ match }) {
+function MovieDetails() {
   const [movie, setMovie] = useState(null);
   const { id } = useParams();
 
-  const findMovie = async () => {
-    await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9ca3c614db35b955a2ca1033fe02b80b`)
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-      setMovie(json);
-    })
-  }
-
-  const callBack = useCallback(() => {
+  useEffect(() => {
+    const findMovie = async () => {
+      await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9ca3c614db35b955a2ca1033fe02b80b`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setMovie(json);
+      })
+    };
     findMovie();
-  }, [])
+  
+  }, [id]);
+  // const findMovie = async () => {
+  //   await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9ca3c614db35b955a2ca1033fe02b80b`)
+  //   .then((res) => res.json())
+  //   .then((json) => {
+  //     console.log(json);
+  //     setMovie(json);
+  //   })
+  // }
+
+  // const callBack = useCallback(() => {
+  //   findMovie();
+  // }, []);
 
 
-  const effect = useEffect(() => {
-      callBack();
-  }, [callBack]);
+  // const effect = useEffect(() => {
+  //     callBack();
+  // }, [callBack]);
 
-  console.log({effect})
+  // console.log({effect});
 
   if (!movie) {
     // Render a loading message or component while fetching movie details
