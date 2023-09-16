c10c71202b9793af2e57f8debdcc879c46551663
Pronothurah <onsongoonditi@gmail.com>
2023-09-17 01:37:32 +0300

    local

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 
 export const AppContext = createContext({});
@@ -27,12 +27,25 @@ export function ContextProvider({ children }) {
         getMovie();
     }
 
-    const memo = useCallback(
-       getMovie, [searchTerm]
-    )
+    // const memo = useCallback(
+    //    getMovie, [searchTerm]
+    // )
     useEffect(() => {
+        const getMovie = async () => {
+            setIsLoading(true);
+            await fetch(`https://api.themoviedb.org/3/search/movie?api_key=9ca3c614db35b955a2ca1033fe02b80b&query=${searchTerm}`)
+            .then((res) => res.json())
+            .then( async (json) => {
+                await setSearchResults(json.results);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setIsLoading(false);
+            });
+        };
         getMovie();
-    }, [memo]);
+    }, [searchTerm]);
 
     return (
         <AppContext.Provider value={{setSearchTerm, searchResults, setSearchResults,isLoading, setIsLoading, handleSearchSubmit, searchTerm, }}>
